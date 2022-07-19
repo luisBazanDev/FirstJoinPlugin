c427e67a04d845590957e4f8c3720df636d73043
Luis Bazán <luis@bazan.pe>
2022-07-19 15:09:09 -0500

    Class of the db system

---
+++ src/main/java/pe/bazan/luis/plugins/firstjoinplugin/PlayerJoined.java
@@ -0,0 +1,41 @@
+package pe.bazan.luis.plugins.firstjoinplugin;
+
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.entity.Player;
+
+public class PlayerJoined {
+  private String name;
+  private Long firstJoin = null;
+  private Long lastJoin = null;
+
+  public PlayerJoined(String name, Long firt, Long last) {
+    this.name = name;
+    this.firstJoin = firt;
+    this.lastJoin = last;
+  }
+
+  public Player getPlayer() {
+    return Bukkit.getPlayer(name);
+  }
+
+  public boolean isFirstJoin() {
+    return this.firstJoin == null;
+  }
+
+  public Long getFirstJoin() {
+    return firstJoin;
+  }
+
+  public Long getLastJoin() {
+    return lastJoin;
+  }
+
+  public static PlayerJoined getInstance(CustomYML db, String name) {
+    return new PlayerJoined(
+            name,
+            db.getConfigField(name + ".firstJoin"),
+            db.getConfigField(name + ".lastJoin")
+    );
+  }
+}
