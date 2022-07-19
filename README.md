ae5db2839efb9076ff8f65f05581bfcdbf4074ba
Luis Bazán <luis@bazan.pe>
2022-07-19 14:32:00 -0500

    Start the project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# First Join Plugin
+A minecraft plugin to be able to easily configure basic things that are executed when a player logs in
+for the first time
---
+++ src/main/java/pe/bazan/luis/plugins/firstjoinplugin/FirstJoinPlugin.java
@@ -0,0 +1,17 @@
+package pe.bazan.luis.plugins.firstjoinplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FirstJoinPlugin extends JavaPlugin {
+
+  @Override
+  public void onEnable() {
+    // Plugin startup logic
+
+  }
+
+  @Override
+  public void onDisable() {
+    // Plugin shutdown logic
+  }
+}
