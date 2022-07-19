74e4e9b92398eabad29976bc43274cbf1ece7b23
Luis Bazán <luis@bazan.pe>
2022-07-19 15:27:08 -0500

    Listen PlayerJoin event and filterring this.

---
+++ src/main/java/pe/bazan/luis/plugins/firstjoinplugin/listeners/PlayerJoin.java
@@ -0,0 +1,29 @@
+package pe.bazan.luis.plugins.firstjoinplugin.listeners;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import pe.bazan.luis.plugins.firstjoinplugin.FirstJoinPlugin;
+import pe.bazan.luis.plugins.firstjoinplugin.PlayerJoined;
+
+public class PlayerJoin implements Listener {
+  private FirstJoinPlugin plugin;
+
+  public PlayerJoin(FirstJoinPlugin plugin) {
+    this.plugin = plugin;
+    plugin.getServer().getPluginManager().registerEvents(this, plugin);
+  }
+
+  @EventHandler
+  public void onPlayerJoin(PlayerJoinEvent e) {
+    final PlayerJoined playerJoined = PlayerJoined.getInstance(plugin.getDb(), e.getPlayer().getName());
+    if(!playerJoined.isFirstJoin()) {
+      playerJoined.saveLast(plugin.getDb());
+      return;
+    }
+    playerJoined.saveFirst(plugin.getDb());
+    plugin.getServer().getPluginManager().callEvent(new PlayerFirstJoin.Event(playerJoined));
+  }
+}
